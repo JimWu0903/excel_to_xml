17bfa28e89bedc24c4f2cd337591bb4a98e71fbc
jim.wu <jim.wu@promeritage.com.tw>
2024-03-19 10:49:50 +0800

    feat: 新增方法將物件寫入 xml

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,16 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.example.blueprint.FoodInfo;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Text;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -17,14 +26,13 @@ public class Main {
     //https://www.baeldung.com/java-convert-excel-data-into-list
 
     private static final String FILE_LOCATION = "/Users/jim.wu/Downloads/222.xlsx";
+    private static final String XML_FILE_LOCATION = "/Users/jim.wu/Downloads/222.xml";
 
     public static void main(String[] args) throws IOException {
 
         System.out.printf("Hello and welcome!");
         List<FoodInfo> result = excelDataToListOfObjets(FILE_LOCATION);
-        for (FoodInfo foodInfo : result) {
-            System.out.println(foodInfo.getCategory() + " " + foodInfo.getName() + " " + foodInfo.getMeasure() + " " + foodInfo.getCalories());
-        }
+        writeFoodInfoListToXml(result, XML_FILE_LOCATION);
     }
 
     public static List<FoodInfo> excelDataToListOfObjets(String fileLocation)
@@ -47,5 +55,58 @@ public class Main {
             foodData.add(foodInfo);
         }
         return foodData;
+    }
+
+    public static void writeFoodInfoListToXml(List<FoodInfo> foodInfoList, String xmlFilePath) {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.newDocument();
+
+            // Create root element,名稱自定義
+            Element rootElement = document.createElement("FoodInfoList");
+            document.appendChild(rootElement);
+
+            for (FoodInfo foodInfo : foodInfoList) {
+                // 每一 row 的 root,名稱自定義
+                Element foodElement = document.createElement("row");
+                rootElement.appendChild(foodElement);
+
+                // Add category
+                Element categoryElement = document.createElement("Category");
+                Text categoryText = document.createTextNode(foodInfo.getCategory());
+                categoryElement.appendChild(categoryText);
+                foodElement.appendChild(categoryElement);
+
+                // Add name
+                Element nameElement = document.createElement("Name");
+                Text nameText = document.createTextNode(foodInfo.getName());
+                nameElement.appendChild(nameText);
+                foodElement.appendChild(nameElement);
+
+                // Add measure
+                Element measureElement = document.createElement("Measure");
+                Text measureText = document.createTextNode(foodInfo.getMeasure());
+                measureElement.appendChild(measureText);
+                foodElement.appendChild(measureElement);
+
+                // Add calories
+                Element caloriesElement = document.createElement("Calories");
+                Text caloriesText = document.createTextNode(String.valueOf(foodInfo.getCalories()));
+                caloriesElement.appendChild(caloriesText);
+                foodElement.appendChild(caloriesElement);
+            }
+
+            // Write to XML file
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(document);
+            StreamResult result = new StreamResult(xmlFilePath);
+            transformer.transform(source, result);
+
+            System.out.println("FoodInfo data written to " + xmlFilePath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
